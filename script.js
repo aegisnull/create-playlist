a57396f3776df7ecde6149afdfe65150a926e043
Luis Tellez <luis@luistellez.com>
2022-05-20 16:43:17 -0700

    Disable Delete playlist button

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let songsContainer = container.querySelector(".songs-container");
 let songs = songsContainer.querySelectorAll(".song");
 let addButton = document.querySelector(".form__submit-btn_action_add");
 let resetButton = document.querySelector(".form__submit-btn_action_reset");
+
+// Disable 'Delete playlist' button if there are no songs
+if (songs.length === 0) {
+  setAttribute("disabled", true);
+}
