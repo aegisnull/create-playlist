5c643c4eaf427572577885bdcf3eab958c78f130
Luis Tellez <luis@luistellez.com>
2022-05-20 12:52:25 -0700

    Declare initial variables

---
+++ script.js
@@ -0,0 +1,4 @@
+// Declare initial variables
+let container = document.querySelector(".container");
+let songsContainer = container.querySelector(".songs-container");
+let songs = songsContainer.querySelectorAll(".song");
