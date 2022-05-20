2751e2ddffd0db0eb73ecb4ee4a61e0ae0ff553c
Luis Tellez <luis@luistellez.com>
2022-05-20 13:00:59 -0700

    Add button variables

--- script.js
+++ script.js
@@ -2,3 +2,5 @@
 let container = document.querySelector(".container");
 let songsContainer = container.querySelector(".songs-container");
 let songs = songsContainer.querySelectorAll(".song");
+let addButton = document.querySelector(".form__submit-btn_action_add");
+let resetButton = document.querySelector(".form__submit-btn_action_reset");
